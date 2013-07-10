10268a55f4f1b21308e7d2334308f8807aac6fae
Felipe Gorigoitia <felipe.gorigoitia@gmail.com>
2013-07-10 04:36:19 +0000

    modify web.js to read index.html

--- web.js
+++ web.js
@@ -5,7 +5,9 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 	fs.readFile('index.html', function (err, data) {
   	if (err) throw err;
- 	  response.send(data);
+ 	  	buf = new Buffer(data);
+		
+		response.send(buf.toString('utf8', 0));
 	});
 //  response.send('Hello World 2!');
 });
